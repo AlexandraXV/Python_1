0aae61a84923bf8afdc831f010949100265b5f60
AlexandraXV <fkbyf1999@rambler.ru>
2024-06-05 15:59:58 +0300

    first hw

---
+++ HW_1/hw_1_task_6.py
@@ -0,0 +1,31 @@
+def word_a():
+    print(1, end="")
+
+def word_b():
+    print(2, end="")
+
+def word_c():
+    print(3, end="")
+
+def word_d():
+    print(4, end="")
+
+def word_f():
+    print(5, end="")
+
+def word_i():
+    print(6, end="")
+
+def word_j():
+    print(7, end="")
+
+def word_k():
+    print(8, end="")
+
+def word_l():
+    print(9, end="")
+
+def word_m():
+    print(0, end="")
+
+word_k(), word_k(), word_m(), word_m(), word_f(), word_f(), word_f(), word_c(), word_f(), word_c(), word_f()
