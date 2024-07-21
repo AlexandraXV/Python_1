2760a200083e88dc040a2e4df577e897ae836fb9
AlexandraXV <fkbyf1999@rambler.ru>
2024-07-21 13:08:20 +0300

    hw_6

---
+++ HW_6/task_1.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+driver.get("http://uitestingplayground.com/ajax")
+driver.implicitly_wait(16)
+
+button = driver.find_element(By.CSS_SELECTOR, "#ajaxButton")
+button.click()
+
+txt = driver.find_element(By.CSS_SELECTOR, ".bg-success").text
+print(txt)
+driver.quit
