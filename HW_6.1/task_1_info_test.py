6790ff5fa580e38a762d397b122f470b3889c3b1
AlexandraXV <fkbyf1999@rambler.ru>
2024-08-02 12:14:41 +0300

    hw_6.1

--- HW_6.1/task_1_info_test.py
+++ HW_6.1/task_1_info_test.py
@@ -1,44 +1,49 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import sleep
 
 driver = webdriver.Chrome()
 driver.maximize_window()
+
 
 def test_data_farm():
     driver.implicitly_wait(10)
     driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
 
-    first_name = driver.find_element(By.CSS_SELECTOR, 'input[name="first-name"]')
+    first_name = driver.find_element(
+        By.CSS_SELECTOR, 'input[name="first-name"]')
     first_name.send_keys("Иван")
-    
+
     last_name = driver.find_element(By.CSS_SELECTOR, 'input[name="last-name"]')
     last_name.send_keys("Петров")
-    
+
     address = driver.find_element(By.CSS_SELECTOR, 'input[name="address"]')
     address.send_keys("Ленина, 55-3")
-    
+
     email = driver.find_element(By.CSS_SELECTOR, 'input[name="e-mail"]')
     email.send_keys("test@skypro.com")
-    
+
     phone = driver.find_element(By.CSS_SELECTOR, 'input[name="phone"]')
     phone.send_keys("+7985899998787")
     city = driver.find_element(By.CSS_SELECTOR, 'input[name="city"]')
     city.send_keys("Москва")
-    
+
     country = driver.find_element(By.CSS_SELECTOR, 'input[name="country"]')
     country.send_keys("Россия")
-    
-    job_position = driver.find_element(By.CSS_SELECTOR, 'input[name="job-position"]')
+
+    job_position = driver.find_element(
+        By.CSS_SELECTOR, 'input[name="job-position"]')
     job_position.send_keys("QA")
-    
-    company = driver.find_element(By.CSS_SELECTOR, 'input[name="company"]')
+
+    company = driver.find_element(
+        By.CSS_SELECTOR, 'input[name="company"]')
     company.send_keys("SkyPro")
-    
-    sleep(3)
-    
-    driver.find_element(By.CSS_SELECTOR, ".btn.btn-outline-primary.mt-3").click()
-    assert "danger" in driver.find_element(By.CSS_SELECTOR, ".alert.py-2.alert-danger").get_attribute("class")
-    assert "success" in driver.find_element(By.CSS_SELECTOR, ".alert.py-2.alert-success").get_attribute("class")
+
+    driver.find_element(
+        By.CSS_SELECTOR, ".btn.btn-outline-primary.mt-3").click()
+    assert "danger" in driver.find_element(
+        By.CSS_SELECTOR, ".alert.py-2.alert-danger").get_attribute("class")
+
+    assert "success" in driver.find_element(
+        By.CSS_SELECTOR, ".alert.py-2.alert-success").get_attribute("class")
 
     driver.quit()
--- HW_6.1/task_3_test.py
+++ HW_6.1/task_3_test.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,25 +10,29 @@ driver.get("https://www.saucedemo.com/")
 
 
 def test_data_farm():
-    driver.find_element(By.CSS_SELECTOR, "#user-name").send_keys("standard_user")
+    driver.find_element(
+        By.CSS_SELECTOR, "#user-name").send_keys("standard_user")
     driver.find_element(By.CSS_SELECTOR, "#password").send_keys("secret_sauce")
     driver.find_element(By.CSS_SELECTOR, "#login-button").click()
-    sleep(3)
-    driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-backpack").click()
-    driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-bolt-t-shirt").click()
-    driver.find_element(By.CSS_SELECTOR, "#add-to-cart-sauce-labs-onesie").click()
-    sleep(3)
+
+    driver.find_element(
+        By.CSS_SELECTOR, "#add-to-cart-sauce-labs-backpack").click()
+    driver.find_element(
+        By.CSS_SELECTOR, "#add-to-cart-sauce-labs-bolt-t-shirt").click()
+    driver.find_element(
+        By.CSS_SELECTOR, "#add-to-cart-sauce-labs-onesie").click()
+
     driver.find_element(By.CSS_SELECTOR, ".shopping_cart_link").click()
     driver.find_element(By.CSS_SELECTOR, "#checkout").click()
     driver.find_element(By.CSS_SELECTOR, "#first-name").send_keys("Alina")
     driver.find_element(By.CSS_SELECTOR, "#last-name").send_keys("Kulikova")
     driver.find_element(By.CSS_SELECTOR, "#postal-code").send_keys("190000")
-    sleep(3)
+
     driver.find_element(By.CSS_SELECTOR, "#continue").click()
-    sleep(3)
 
     wait = WebDriverWait(driver, 10)
     assert wait.until(
-    EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".summary_total_label"), "$58.29")
+        EC.text_to_be_present_in_element(
+            (By.CSS_SELECTOR, ".summary_total_label"), "$58.29")
     )
     driver.quit()
